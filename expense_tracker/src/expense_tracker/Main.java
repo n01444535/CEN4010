65459af54942acb76b5857591909edea3f21de26
n01444535 <n01444535@unf.edu>
2025-02-13 10:47:41 -0500

    None

--- expense_tracker/src/expense_tracker/Main.java
+++ expense_tracker/src/expense_tracker/Main.java
@@ -5,7 +5,7 @@ import view.LoginForm;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseManager.initialize(); // Đảm bảo database được thiết lập
-        new LoginForm(); // Khởi chạy giao diện đăng nhập
+        DatabaseManager.initialize();
+        new LoginForm();
     }
 }
